8a1ad68327d2f4c7f423b0961672bc6f26ef29a5
Han Wang <hyw2@cornell.edu>
2021-05-12 23:14:18 -0400

    Add initial setup files

--- App.tsx
+++ App.tsx
@@ -1,11 +1,166 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  ImageBackground,
+} from 'react-native';
+import { Camera } from 'expo-camera';
+import Constants from 'expo-constants';
 
+let camera: Camera;
 export default function App() {
+  const [isUsingCamera, setIsUsingCamera] = React.useState(false);
+  const [previewVisible, setPreviewVisible] = React.useState(false);
+  const [capturedImage, setCapturedImage] = React.useState<any>(null);
+
+  const startCamera = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    if (status === 'granted') {
+      setIsUsingCamera(true);
+    } else {
+      Alert.alert('Access denied');
+    }
+  };
+  const takePicture = async () => {
+    const photo: any = await camera.takePictureAsync();
+    setPreviewVisible(true);
+    setCapturedImage(photo);
+
+    let localUri = photo.uri;
+    let filename = localUri.split('/').pop();
+    // Infer the type of the image
+    let match = /\.(\w+)$/.exec(filename);
+    let type = match ? `image/${match[1]}` : `image`;
+
+    // Upload the image using the fetch and FormData APIs
+    let formData = new FormData();
+    // @ts-ignore
+    formData.append('photo', { uri: localUri, name: filename, type });
+
+    try {
+      const { manifest } = Constants;
+      const uri = `http://${manifest.debuggerHost.split(':').shift()}:5000`;
+      const result = await fetch(`${uri}/api/photo/`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      });
+      const resultData = await result.json();
+      console.log(resultData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+    startCamera();
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {isUsingCamera ? (
+        <View
+          style={{
+            flex: 1,
+            width: '100%',
+          }}
+        >
+          {previewVisible && capturedImage ? (
+            <CameraPreview
+              photo={capturedImage}
+              retakePicture={retakePicture}
+            />
+          ) : (
+            <Camera
+              type={Camera.Constants.Type.back}
+              style={{ flex: 1 }}
+              ref={(r) => {
+                if (r != null) camera = r;
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  width: '100%',
+                  backgroundColor: 'transparent',
+                  flexDirection: 'row',
+                }}
+              >
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    flexDirection: 'row',
+                    flex: 1,
+                    width: '100%',
+                    padding: 20,
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <View
+                    style={{
+                      alignSelf: 'center',
+                      flex: 1,
+                      alignItems: 'center',
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={takePicture}
+                      style={{
+                        width: 70,
+                        height: 70,
+                        bottom: 0,
+                        borderRadius: 50,
+                        backgroundColor: '#fff',
+                      }}
+                    />
+                  </View>
+                </View>
+              </View>
+            </Camera>
+          )}
+        </View>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <TouchableOpacity
+            onPress={startCamera}
+            style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: '#14274e',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 40,
+            }}
+          >
+            <Text
+              style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +174,59 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const CameraPreview = ({ photo, retakePicture }: any) => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'transparent',
+        flex: 1,
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <ImageBackground
+        source={{ uri: photo && photo.uri }}
+        style={{
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            padding: 15,
+            justifyContent: 'flex-end',
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <TouchableOpacity
+              onPress={retakePicture}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4,
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20,
+                }}
+              >
+                Re-take photo
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
